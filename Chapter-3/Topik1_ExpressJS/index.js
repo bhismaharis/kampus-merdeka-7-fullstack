e40205e71fafc4d31bd00fa3ed511491f7a6467b
bhismaharis <bhismaharis11@gmail.com>
2024-10-01 23:25:06 +0700

    [09] Feature: Chapter 3 Topik 2, method get & post dan postman

--- Chapter-3/Topik1_ExpressJS/index.js
+++ Chapter-3/Topik1_ExpressJS/index.js
@@ -5,6 +5,9 @@ const students = require('./data/students.json'); // Import students data
 const app = express(); // Create express app
 const port = 3000; // Define port
 
+// We need to activate body parser/reader
+app.use(express.json()); // Activate body reader
+
 // Make a routing and response
 app.get('/', (req, res) => { // Define route
     res.send('Hello World!'); // Send response
@@ -37,6 +40,94 @@ app.get('/students/:id', (req, res) => {
 
     // if there is no student with the id that client request, it will response not found
     res.status(404).json({message: 'Student not found!'});
+});
+
+app.post('/students', (req, res) => { // Define route
+    const {name, nickname, address, education} = req.body; // Get data from request body
+    if (!name || name == "") { // If name is empty
+        res.status(400).json({
+            message: 'Name is required!'
+        }); // Send response
+        return; // Stop
+    }
+    if (!nickname || nickname == "") { // If nickname is empty
+        res.status(400).json({
+            message: 'Nickname is required!'
+        }); // Send response
+        return; // Stop
+    }
+    if (!req.body.class || req.body.class == "") { // If class is empty
+        res.status(400).json({
+            message: 'Class is required'
+        }); // Send response
+        return; // Stop
+    }
+    if (!address || address == "") { // If address is empty
+        res.status(400).json({
+            message: 'Address is required!'
+        }); // Send response
+        return; // Stop
+    }
+    if (!education || education == "") { // If education is empty
+        res.status(400).json({
+            message: 'Education is required!'
+        }); // Send response
+        return; // Stop
+    }
+
+    const { province, city } = address; // Get address data
+    if (!province || province == "") {
+      // If province is empty
+        res.status(400).json({
+        message: "Province is required!",
+      }); // Send response
+      return; // Stop
+    }
+    if (!city || city == "") { // If city is empty
+        res.status(400).json({
+            message: 'City is required!'
+        }); // Send response
+        return; // Stop
+    }
+
+    const {bachelor} = education; // Get education data
+    if (!bachelor || bachelor == "") { // If bachelor is empty
+        res.status(400).json({
+            message: 'Bachelor is required!'
+        }); // Send response
+        return; // Stop
+    }
+
+    const id = students.length + 1; // Generate new id
+    const newStudent = {
+        id,
+        name,
+        nickname,
+        class: req.body.class,
+        address: {
+            province,
+            city,
+        },
+        education: {
+            bachelor,
+        },
+    }; // Create new student
+
+    students.push(newStudent); // Add new student to students data
+
+    // Save new student to student.json file
+    // const fs = require('fs'); // Import fs module
+    // fs.writeFileSync('./data/students.json', JSON.stringify(students, null, 2)); // Write students data to file
+
+    // using fs and path module
+    const fs = require('fs'); // Import fs module
+    const path = require('path'); // Import path module
+    const filePath = path.join(__dirname, 'data', 'students.json'); // Define file path
+    fs.writeFileSync
+    (filePath, JSON.stringify(students, null, 2)); // Write students data to file
+    
+
+    res.status(201).json(newStudent); // Send response
 });
 
 
