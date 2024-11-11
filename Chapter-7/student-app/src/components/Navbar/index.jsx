75744460f0cdb5058b3c7f47ee94278dd7e04fea
bhismaharis <bhismaharis11@gmail.com>
2024-11-11 20:49:16 +0700

    [17.1] Feature: Chapter 7 Topik 1, duplicate file and make using tanstack query in navbar

---
+++ Chapter-7/student-app/src/components/Navbar/index.jsx
@@ -0,0 +1,97 @@
+import { Link, useNavigate } from "@tanstack/react-router";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setToken, setUser } from "../../redux/slices/auth";
+import { useQuery } from "@tanstack/react-query";
+import { profile } from "../../service/auth";
+
+const NavigationBar = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const { user, token } = useSelector((state) => state.auth);
+
+    const handleLogout = useCallback(() => {
+        // delete the local storage here
+        dispatch(setUser(null));
+        dispatch(setToken(null));
+
+        // redirect to login
+        navigate({ to: "/login" });
+    }, [dispatch, navigate]);
+
+    const { data, isSuccess, isError } = useQuery({
+        queryKey: "profile",
+        queryFn: profile,
+        enabled: token ? true : false,
+    });
+
+    useEffect(() => {
+        if (isSuccess) {
+            dispatch(setUser(data?.data));
+        } else if (isError) {
+            handleLogout();
+        }
+    }, [data, dispatch, handleLogout, isError, isSuccess]);
+
+    const logout = (event) => {
+        event.preventDefault();
+        handleLogout();
+    };
+
+    return (
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    Student Wakanda App
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        {user && user?.role_id === 1 && (
+                            <Nav.Link as={Link} to="/students/create">
+                                Create Student
+                            </Nav.Link>
+                        )}
+                    </Nav>
+                    <Nav>
+                        {user ? (
+                            <>
+                                <Nav.Link as={Link} to="/profile">
+                                    <Image
+                                        src={user?.profile_picture}
+                                        fluid
+                                        style={{
+                                            width: "30px",
+                                            height: "30px",
+                                            display: "inline-block",
+                                            overflow: "hidden",
+                                            borderRadius: "50%",
+                                        }}
+                                    />{" "}
+                                    {user?.name}
+                                </Nav.Link>
+                                <Nav.Link onClick={logout}>Logout</Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <Nav.Link as={Link} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/register">
+                                    Register
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavigationBar;
---
+++ Chapter-7/student-app/src/service/student/index.js
@@ -0,0 +1,113 @@
+export const getStudents = async (nickname, name) => {
+    const token = localStorage.getItem("token");
+    let params;
+    if (nickname) {
+        params.append("nickname", nickname);
+    }
+    if (name) {
+        params.append("name", name);
+    }
+
+    let url =
+        `${import.meta.env.VITE_API_URL}/students` +
+        new URLSearchParams(params);
+
+    const response = await fetch(url, {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        method: "GET",
+    });
+
+    // get the data if fetching succeed!
+    const result = await response.json();
+    return result;
+};
+
+export const getDetailStudent = async (id) => {
+    const token = localStorage.getItem("token");
+
+    let url = `${import.meta.env.VITE_API_URL}/students/${id}`;
+
+    const response = await fetch(url, {
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        method: "GET",
+    });
+
+    // get data
+    const result = await response.json();
+    return result;
+};
+
+export const createStudent = async (request) => {
+    const token = localStorage.getItem("token");
+
+    const formData = new FormData();
+    formData.append("name", request.name);
+    formData.append("nick_name", request.nickName);
+    formData.append("class_id", request.classId);
+    formData.append("university_id", request.universityId);
+    if (request.profilePicture) {
+        formData.append("profile_picture", request.profilePicture);
+    }
+
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/students`, {
+        body: formData,
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        method: "POST",
+    });
+
+    // get the data if fetching succeed!
+    const result = await response.json();
+    return result;
+};
+
+export const updateStudent = async (id, request) => {
+    const token = localStorage.getItem("token");
+
+    const formData = new FormData();
+    formData.append("name", request.name);
+    formData.append("nick_name", request.nickName);
+    formData.append("class_id", request.classId);
+    formData.append("university_id", request.universityId);
+    if (request.profilePicture) {
+        formData.append("profile_picture", request.profilePicture);
+    }
+
+    const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/students/${id}`,
+        {
+            body: formData,
+            headers: {
+                authorization: `Bearer ${token}`,
+            },
+            method: "PUT",
+        }
+    );
+
+    // get the data if fetching succeed!
+    const result = await response.json();
+    return result;
+}
+
+export const deleteStudent = async (id) => {
+    const token = localStorage.getItem("token");
+
+    const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/students/${id}`,
+        {
+            headers: {
+                authorization: `Bearer ${token}`,
+            },
+            method: "DELETE",
+        }
+    );
+
+    // get the data if fetching succeed!
+    const result = await response.json();
+    return result;
+};
